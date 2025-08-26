09cfbeb9271e672c1e444197074917d5cf90b28c
Anthony <d.a.burchfield@att.net>
2025-08-25 20:55:50 -0500

    Added code for the main routine.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+// Entry point
+class Program
+{
+    static void Main(string[] args)
+    {
+        var sourceFolder = "src"; // Change as needed
+        var buildFolder = "build";
+        var compiler = new PumaCompiler(sourceFolder, buildFolder);
+        compiler.Compile();
+        Console.WriteLine("Compilation complete.");
+    }
+}
